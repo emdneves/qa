ef99b18e0fa8bf04a7b35a2b057a99955fe15883
emanuel <ima7@live.com>
2023-06-07 16:33:55 +0100

    PL01 ex 2

---
+++ PL01/src/main/java/org/PL01_02/Main.java
@@ -0,0 +1,10 @@
+package org.PL01_02;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(StringUtils.isPalindrome("abccba"));
+        System.out.println(StringUtils.isPalindrome("abcdef"));
+
+    }
+}
