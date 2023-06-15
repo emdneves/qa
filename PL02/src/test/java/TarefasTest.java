84ff63ccd29b3f4628e48f84ee23da2287bb5c0c
emanuel neves <ima7@live.com>
2023-06-15 17:24:49 +0100

    ex 03 pl 02

--- PL02/src/test/java/TarefasTest.java
+++ PL02/src/test/java/TarefasTest.java
@@ -4,47 +4,52 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class TarefasTest {
-    private Ano ano1;
-    private Mes mes1;
-    private Dia dia1;
-    private ArrayList<String> tarefas;
 
-    @BeforeEach
-    public void setUp() {
-        ano1 = new Ano(1993);
-        mes1 = new Mes("Maio");
-        dia1 = new Dia(20);
-        tarefas = new ArrayList<>();
+        @Test
+        public void testMesGetNome() {
+            Mes mes = new Mes("Janeiro");
+            assertEquals("Janeiro", mes.getNome());
+        }
+
+        @Test
+        public void testDiaGetDia() {
+            Dia dia = new Dia(15);
+            assertEquals(15, dia.getDia());
+        }
+
+        @Test
+        public void testDiaAdicionarTarefa() {
+            Dia dia = new Dia(20);
+            dia.adicionarTarefa("Tarefa 1");
+            dia.adicionarTarefa("Tarefa 2");
+            assertEquals(2, dia.getTarefas().size());
+        }
+
+        @Test
+        public void testAnoGetAno() {
+            Ano ano = new Ano(2023);
+            assertEquals(2023, ano.getAno());
+        }
+
+        @Test
+        public void testAnoGetMeses() {
+            Ano ano = new Ano(2023);
+            Mes[] meses = ano.getMeses();
+            assertEquals(12, meses.length);
+        }
+
+        @Test
+        public void testObterNomeMes() {
+            Ano ano = new Ano(2023);
+            Mes mes = new Mes("janeiro");
+
+        }
     }
 
-        @Test
-        public void testGeters () {
-            assertEquals(1993, ano1.getAno());
-            assertEquals("Maio", mes1.getNome());
-            assertEquals(20, dia1.getDia());
-        }
-
-        @Test
-        public void testAdicionarTarefa () {
-        tarefas.add(1, "abc");
-        assertTrue(tarefas.contains("abc"));
-        }
-
-        @Test
-        public void testGetTarefas(){
-            tarefas.add(1, "abc");
-            assertTrue(tarefas.contains("abc"));
-        }
-
-        @Test
-        public void testGetDias() {
-            assertEquals(31, mes1.size(getDias());
-        }
 
 
 
@@ -54,5 +59,4 @@ public class TarefasTest {
 
 
 
-}
 
